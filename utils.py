6ac96fbf4bd7d89e70484dc6587481677e922c40
Sergio Parada <sdparada97@gmail.com>
2023-05-30 16:01:35 -0500

    feature: add the menu of the app

---
+++ utils.py
@@ -0,0 +1,23 @@
+def get_all_students_by_city():
+    pass
+
+def get_all_students_by_country():
+    pass
+
+def get_all_students_by_age():
+    pass
+
+def get_all_hometowns():
+    pass
+
+def get_average_age_by_career():
+    pass
+
+def get_average_status_student_by_career():
+    pass
+
+def get_group_students_with_ranges_age():
+    pass
+
+def get_the_most_city_with_careers():
+    pass
