8bc533001858f9044653d7a7e33ac9e689a6bbed
Sergio Parada <sdparada97@gmail.com>
2023-06-02 13:32:03 -0500

    feature: it has been finished project

--- utils.py
+++ utils.py
@@ -3,86 +3,125 @@ from pprint import pprint
 from unicodedata import normalize
 from colorama import Fore, Style
 
+
 def csv_to_dict():
-    with open('data.csv', mode='r') as csv_file:
+    with open("data.csv", mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return list(csv_reader)
 
+
 DICT_CSV = csv_to_dict()
 
+
 def clean_string(sentence):
-    normal_str = normalize('NFKD',sentence)
-    clean_str = normal_str.encode('ASCII', 'ignore').decode('ASCII')
+    normal_str = normalize("NFKD", sentence)
+    clean_str = normal_str.encode("ASCII", "ignore").decode("ASCII")
     return clean_str.casefold()
 
-def get_all_students_by_city():
-    city_input = input(f'{Fore.BLUE}Ingrese la ciudad para su consulta: {Style.RESET_ALL}')
 
-    students_by_city = [
-        dict
-        for dict in DICT_CSV
-        if city_input.lower() == clean_string(dict['ciudad'])
+def get_all_students_by_city():
+    city_input = input(
+        f"{Fore.BLUE}Ingrese la ciudad para su consulta: {Style.RESET_ALL}"
+    )
+
+    return [
+        dict for dict in DICT_CSV if city_input.lower() == clean_string(dict["ciudad"])
     ]
-    pprint(students_by_city)
+
 
 def get_all_students_by_country():
-    country_input = input(f'{Fore.BLUE}Ingrese el pais para su consulta: {Style.RESET_ALL}')
+    country_input = input(
+        f"{Fore.BLUE}Ingrese el pais para su consulta: {Style.RESET_ALL}"
+    )
 
-    students_by_country = [
-        dict
-        for dict in DICT_CSV
-        if country_input.lower() == clean_string(dict['pais'])
+    return [
+        dict for dict in DICT_CSV if country_input.lower() == clean_string(dict["pais"])
     ]
-    pprint(students_by_country)
+
 
 def get_all_students_by_age():
-    range_age_input = input(f'{Fore.BLUE}Ingrese el rango de edad para su consuta (edad-edad): {Style.RESET_ALL}').split('-')
+    range_age_input = input(
+        f"{Fore.BLUE}Ingrese el rango de edad para su consuta (edad-edad): {Style.RESET_ALL}"
+    ).split("-")
 
-    students_by_age = [
+    return [
         dict
         for dict in DICT_CSV
-        if range_age_input[0] <= dict['edad'] <= range_age_input[1]
+        if range_age_input[0] <= dict["edad"] <= range_age_input[1]
     ]
-    pprint(students_by_age)
+
 
 def get_all_hometowns():
     cities_of_students = []
     for dict in DICT_CSV:
-        if dict['ciudad'] not in cities_of_students:
-            cities_of_students.append(dict['ciudad'])            
-    pprint(cities_of_students)
+        if dict["ciudad"] not in cities_of_students:
+            cities_of_students.append(dict["ciudad"])
+
+    return cities_of_students
+
 
 def get_careers():
     careers_of_student = []
     for dict in DICT_CSV:
-        if dict['carrera'] not in careers_of_student:
-            careers_of_student.append(dict['carrera'])
+        if dict["carrera"] not in careers_of_student:
+            careers_of_student.append(dict["carrera"])
     return careers_of_student
+
 
 def get_average_age_by_career():
     careers = get_careers()
-    averages_age = []
+    averages_age = {}
     for career in careers:
-        count_std = 0
-        sum_age = 0
+        count_std, sum_age = (0, 0)
         for dict in DICT_CSV:
-            if career == dict['carrera']:
-                sum_age += int(dict['edad'])
-                count_std +=1
-        averages_age.append(int(sum_age/count_std))
+            if career == dict["carrera"]:
+                sum_age += int(dict["edad"])
+                count_std += 1
+        averages_age[career] = int(sum_age / count_std)
 
-    result = {}
-    for key,value in zip(careers,averages_age): 
-        if key not in result:
-            result[key]=value
+    return averages_age
 
-    pprint(result)
 
 def get_average_status_student_by_career():
-    pass
+    averages_by_career = get_average_age_by_career()
+    age_input = int(
+        input(f"{Fore.BLUE}Ingrese la edad del estudiante: {Style.RESET_ALL}")
+    )
+    career_input = input(
+        f"{Fore.BLUE}Ingrese la carrera del estudiante: {Style.RESET_ALL}"
+    )
+
+    print(
+        f"{Fore.GREEN}Esta encima del promedio{Style.RESET_ALL}"
+        if age_input > averages_by_career[career_input]
+        else f"{Fore.RED}Esta debajo del promedio{Style.RESET_ALL}"
+    )
+    return
+
 
 def get_group_students_with_ranges_age():
-    pass
+    options_range = {
+        "1": [dict for dict in DICT_CSV if 18 <= int(dict["edad"]) <= 25],
+        "2": [dict for dict in DICT_CSV if 26 <= int(dict["edad"]) <= 35],
+        "3": [dict for dict in DICT_CSV if int(dict["edad"]) > 35],
+    }
+    return options_range[
+        input(
+            f"{Fore.BLUE}Ingrese alguna de las opciones: \n 1) 18-25\n 2) 26-35\n 3) Mayores a 35\n {Style.RESET_ALL}"
+        )
+    ]
+
 
 def get_the_most_city_with_careers():
-    pass
+    city_result = (None,0)
+    cities = get_all_hometowns()
+    for city in cities:
+        careers_by_city = []
+        for dict in DICT_CSV:
+            if dict['ciudad'] == city:
+                if dict['carrera'] not in careers_by_city:
+                    careers_by_city.append(dict['carrera'])
+        if len(careers_by_city) >= city_result[1]:
+            city_result = (city,len(careers_by_city))
+
+    return city_result
