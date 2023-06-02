537955979940dbf5253e940457e6455fc03d8eee
Sergio Parada <sdparada97@gmail.com>
2023-06-01 20:57:33 -0500

    feature:  add get_all_students_by_city

--- utils.py
+++ utils.py
@@ -1,14 +1,32 @@
 import csv
+from pprint import pprint
+from unicodedata import normalize
+from colorama import Fore, Style
 
 def csv_to_dict():
     with open('data.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return list(csv_reader)
 
-dict_csv = csv_to_dict()
+DICT_CSV = csv_to_dict()
+
+def clean_string(sentence):
+    normal_str = normalize('NFKD',sentence)
+    clean_str = normal_str.encode('ASCII', 'ignore').decode('ASCII')
+    return clean_str.casefold()
 
 def get_all_students_by_city():
-    pass
+    city_input = input(f'{Fore.BLUE}Ingrese la ciudad para su consulta: {Style.RESET_ALL}')
+    
+    students_by_city = []
+    count_students = 0
+    for dict in DICT_CSV:
+        if city_input.lower() == clean_string(dict['ciudad']):
+            students_by_city.append(dict)
+            count_students += 1
+
+    pprint(students_by_city)
+    print(f'{Fore.GREEN}Cantidad de estudiantes en la ciudad {city_input}: {count_students}{Style.RESET_ALL}')
 
 def get_all_students_by_country():
     pass
